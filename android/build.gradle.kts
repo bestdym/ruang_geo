325bf881fe348184cd401e4ba1d1ad58ff6697b2
bestdym <ndimndimmasn@gmail.com>
2026-06-04 12:07:48 +0700

    fix apk

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,31 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+subprojects {
+    val configureAndroid: Project.() -> Unit = {
+        val androidExtension = extensions.findByName("android")
+        if (androidExtension != null) {
+            try {
+                val getNamespace = androidExtension.javaClass.getMethod("getNamespace")
+                val namespace = getNamespace.invoke(androidExtension)
+                if (namespace == null) {
+                    val setNamespace = androidExtension.javaClass.getMethod("setNamespace", String::class.java)
+                    val packageName = "com.ruanggeo." + name.replace("-", "_").replace(":", "_")
+                    setNamespace.invoke(androidExtension, packageName)
+                    println("Automatically set namespace to $packageName for subproject $name")
+                }
+            } catch (e: Exception) {
+                // Ignore if method does not exist (older AGP versions)
+            }
+        }
+    }
+
+    if (state.executed) {
+        configureAndroid()
+    } else {
+        afterEvaluate {
+            configureAndroid()
+        }
+    }
+}
